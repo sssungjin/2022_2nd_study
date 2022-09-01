8fd881c58eb06a7ee86444be3235978a96cda046
sssungjin <dkdlwls576@gmail.com>
2022-09-01 17:11:12 +0900

    1

---
+++ 알골/lab1/1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class  InputNum
+{
+public:
+	InputNum ()
+	{
+		 cout << "Enter number ";
+		 cin >> _num;
+	}
+
+	int GetValue () const { return _num; }
+
+	void AddInput ()
+	{
+		InputNum aNum;  // get a number from user
+		_num = _num + aNum.GetValue ();
+	}
+
+private:
+	int _num;
+};
+int main()
+{
+	InputNum num;
+	cout << "The value is " << num.GetValue() << "\n";
+	num.AddInput();
+	cout << "Now the value is " << num.GetValue() << "\n";
+}
